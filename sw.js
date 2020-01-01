d948f23a21f907e81aca4ca84bda40a134832662
extracold1209 <extracold1209@gmail.com>
2020-01-01 16:31:47 +0900

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-6f7529f28e2c09521469.js"
+    "url": "webpack-runtime-dfc297c1a5f7c4f0e4ac.js"
   },
   {
     "url": "styles.f73c58f4bf8599b792e0.css"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
     "url": "styles-467c764626bd09a8bf6d.js"
   },
   {
-    "url": "commons-31ec095c0dc9fd0224e3.js"
+    "url": "commons-c89b5eeb49b970ca3afd.js"
   },
   {
     "url": "app-007a5213dad975eb00b0.js"
@@ -46,7 +46,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "aceb23e170cfdabca15dccfb2648561b"
+    "revision": "7e5a943d0516c27faeb584887b1edc7a"
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
